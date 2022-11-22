38c244c31111d64b59440562bf23f1d4e27588cb
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2022-11-22 15:03:04 +0100

    add more features

--- _printf.c
+++ _printf.c
@@ -27,8 +27,22 @@ int _printf(const char *format, ...)
 
 		} else
 		{
-			countStr = write(1, "!", 1);
 			i++;
+			if (format[i] == 'c')
+				countStr = write(1, &format[i], 1);
+			else if (format[i] == 's')
+				countStr = write(1, &format[i], 1);
+			else
+			{
+				write(1, "\nerror... '", 10);
+				write(1, "%", 1);
+				write(1, &format[i], 1);
+				write(1, "'", 1);
+				break;
+			}
+			i++;
+
+
 			total += countStr;
 
 		}
