0ada0ca268dd51724dabfec50863645cc5265404
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2022-11-22 12:39:19 +0100

    add an header file with some prototype init

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/* including standard library */
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+/* Prototypes */
+int _printf(const char *format, ...);
+
+#endif /* _MAIN_H_ */
